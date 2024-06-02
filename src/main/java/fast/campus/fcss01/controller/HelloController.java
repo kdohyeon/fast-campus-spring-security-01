42b12c54cb09a1e0b653cae5b6a11c4f647ebe2c
kdohyeon <kdohyeon@gmail.com>
2024-06-02 20:21:30 +0900

    commit

--- src/main/java/fast/campus/fcss01/controller/HelloController.java
+++ src/main/java/fast/campus/fcss01/controller/HelloController.java
@@ -1,5 +1,7 @@
 package fast.campus.fcss01.controller;
 
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/api/v1/hello")
     public String hello() {
+        UserDetails user = User.withUsername("danny.kim")
+                .password("12345")
+                .authorities("READ")
+                .build();
         return "Hello, Spring Security";
     }
 }
